730b541db80c3d74df7be2284045d41cf9ecd054
CatjingY <mingle2669310155@outlook.com>
2024-06-08 16:51:32 +0800

    Site updated: 2024-06-08 16:51:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/05/hello-world/","2024/05/07/红岸自编的浏览器开源项目科学上网傻瓜式搭建教程/"];function toRandomPost(){
+var posts=["2024/06/08/python学习心得①/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
